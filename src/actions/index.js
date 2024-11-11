c175b876ed6040edcc8f77d94148b2ece959b61e
a.allhyani <1498@ntis.com>
2024-11-11 15:37:47 +0300

    Create action creater Action Creater and retern action

---
+++ src/actions/index.js
@@ -0,0 +1,20 @@
+/**
+ * @author 'abdullah allhayni  '
+ * @version v1.0
+ * @description Action Creater and retern action  .
+ * @TODO : selectSong  .
+ * 
+ * @step :
+ * 1- create function
+ * 2- Add type and payload 
+ * 3- return an action 
+ * 4- export *
+
+ */
+export const selectSong = (song) => {
+  // return an action
+  return {
+    type: "SONG_SELECTED",
+    payload: song,
+  };
+};
